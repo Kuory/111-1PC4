f95890a9b10a515851071df81dcf9b3080022569
kuory <108111121@mail.aeust.edu.tw>
2022-10-26 02:06:51 +0800

    1111

---
+++ 111-1PC4/Test1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1PC4
+{
+    public partial class Test1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i_Age = 49;
+            int i_Sex = 0;
+            if (i_Age >= 50)
+            {
+                if (i_Sex == 1)
+                {
+                Response.Write("壯年男子的程式執行程式碼區域")
+                }
+                else
+                {
+                Response.Write("壯年女子的程式執行程式碼區域")
+                }       
+            }
+            else
+            {
+                if (i_Sex == 1)
+                {
+                    Response.Write("年輕男子的程式執行程式碼區域")
+                }
+                else
+                {
+                    Response.Write("年輕女子的程式執行程式碼區域")
+                }
+            }
+        }
+    }
+}
